b284640bc89fd4b08948250b2e5d42d3596a2db4
manufloridoCESUR <manuel.ma39601@cesurformacion.com>
2021-05-19 12:53:22 +0200

    Script final

--- script.sh
+++ script.sh
@@ -13,6 +13,3 @@ free -m >> monitorizacion.txt
 echo "" >> monitorizacion.txt
 echo "Comprobador del Sistema de Archivos" >> monitorizacion.txt
 df -H >> monitorizacion.txt
-
-mkdir /home/usuario/Escritorio/Prueba
-mv monitorizacion.txt /home/usuario/Escritorio/Prueba
