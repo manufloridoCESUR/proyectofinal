968d0e524f753c71caefad02cd2e0ffb1d2ec808
manufloridoCESUR <manuel.ma39601@cesurformacion.com>
2021-05-19 11:04:14 +0200

    Segunda subida del script

---
+++ script.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+echo "Hora del Sistema, Hora de Inicio, Usuarios y Carga del Procesador" > monitorizacion.txt
+uptime >> monitorizacion.txt
+echo "" >> monitorizacion.txt
+echo "Procesos activos del sistema" >> monitorizacion.txt
+ps -a >> monitorizacion.txt
+echo "" >> monitorizacion.txt
+echo "Usuarios conectados al sistema" >> monitorizacion.txt
+w -h >> monitorizacion.txt
+echo "" >> monitorizacion.txt
+echo "Uso de la Memoria Física y de Intercambio" >> monitorizacion.txt
+free -m >> monitorizacion.txt
+echo "" >> monitorizacion.txt
+echo "Comprobador del Sistema de Archivos" >> monitorizacion.txt
+df -H >> monitorizacion.txt
